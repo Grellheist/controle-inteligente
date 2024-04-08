c159dea01c63831e4a44f5032b16f8ef74a0fd91
Grellheist <grellheist@tutanota.com>
2024-04-08 19:47:50 -0300

    changed files

--- sistema_discreto_malha_aberta.py
+++ sistema_discreto_malha_aberta.py
@@ -2,42 +2,30 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import numpy
 
-num = [10]
-den = [1, 2, 1]
+num = [10.]
+den = [1., 1, 3]
 
-#Funcao de transferencia
-lti = signal.TransferFunction(num, den)
-discrete_lti = lti.to_discrete(dt=0.1)
+G = signal.lti(num, den)
+Gd = G.to_discrete(dt=0.1)
 
-#discrete_lti.num=[a_m, a_m-1, ..., a_0 ]
-print(discrete_lti.num)
+a = numpy.flip(Gd.num)
+b = numpy.flip(Gd.den[1:])
 
-#discrete_lti.den=[b_n, b_n-1, ..., b_0 ]
-print(discrete_lti.den)
+tempo = 20
+dt = 0.1
 
-dt=0.1 #Perıodo de amostragem
+qtd_amostras = int(tempo/dt)
 
-tempo_simulacao = 10 #Tempo maximo de simulacao
+maior_tamanho = numpy.max([len(a), len(b)])
 
-num = [3]
-den = [1, 1, 3]
+x = numpy.ones(qtd_amostras+maior_tamanho)
+y = numpy.zeros(qtd_amostras+maior_tamanho)
 
-#Funcao de transferencia
-lti = signal.TransferFunction(num, den)
-discrete_lti = lti.to_discrete(dt)
+for i in range(maior_tamanho,qtd_amostras+maior_tamanho):
+	y[i] = numpy.sum(-1*b*y[i-len(b):i])+numpy.sum(x[i-len(a):i]*a)
 
-a = numpy.flip(discrete_lti.num)
-b = numpy.flip(discrete_lti.den[1:])
-
-tempo = numpy.arange(0,tempo_simulacao,0.1)
-
-maior_janela = numpy.max([len(a), len(b)])
-
-u = numpy.ones(len(tempo) + maior_janela)
-y = numpy.zeros(len(tempo) + maior_janela)
-
-for i in range(maior_janela, len(y), 1):
-    y[i]=-numpy.sum(y[i-len(b):i]*b)
-    +numpy.sum(u[i-len(a):i]*a)
-
-plt.step(tempo, y[maior_janela:])
+plt.step(dt*numpy.arange(0,qtd_amostras),y[maior_tamanho:])
+plt.xlabel('Tempo (Segundos)')
+plt.ylabel('Amplitude')
+plt.legen('Reposta ao degrau do sistema discreto')
+plt.show()
